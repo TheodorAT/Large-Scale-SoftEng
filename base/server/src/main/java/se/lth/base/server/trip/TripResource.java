70d4117679dd354ad7caf69117bfc81e49438172
Magnus Remnetorp <magnus.remnetorp.0016@student.lu.se>
2023-09-20 15:05:54 +0200

    [ETS-895] added a method retrieving list of trips handling HTTP GET requests, return list in JSON format

--- base/server/src/main/java/se/lth/base/server/trip/TripResource.java
+++ base/server/src/main/java/se/lth/base/server/trip/TripResource.java
@@ -3,6 +3,8 @@ package se.lth.base.server.trip;
 import se.lth.base.server.Config;
 import se.lth.base.server.user.*;
 
+import java.util.List;
+
 import javax.ws.rs.*;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.Context;
@@ -23,5 +25,24 @@ public class TripResource {
     public Trip createTrip(Trip trip) {
         Trip result = tripDao.addTrip(user.getId(), trip);
         return result;
+    }
+
+    /**
+     * Retrieve a list of trips matching specified parameters.
+     * 
+     * @param fromLocationId ID of the starting as parameter when searching for the trips.
+     * @param toLocationId   ID of the destination as parameter when searching for the trips.
+     * @return A list of Trip objects representing trips that match the parameters. Returned to the client in JSON format.
+     *
+     */
+    @GET
+    @Path("/search") // Will need to update, (update in test as well)
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Trip> searchTrips(
+            //possible to add parameters below, such as date/time/seats. Needs to be added in method availableTrips (TripDataAccess) as well. (requires also modify respective test method)
+            @QueryParam("fromLocationId") int fromLocationId,
+            @QueryParam("toLocationId") int toLocationId) {
+        List<Trip> matchingTrips = tripDao.availableTrips(fromLocationId, toLocationId);
+        return matchingTrips;
     }
 }
