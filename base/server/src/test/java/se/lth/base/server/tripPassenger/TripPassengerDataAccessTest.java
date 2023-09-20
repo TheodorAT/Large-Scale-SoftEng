5d5e6e4db3455ad158547ef8c0b0ad887dc666f4
antontttt <89006432+antontttt@users.noreply.github.com>
2023-09-20 23:24:46 +0200

    Test for resource file not implemented, suspect that there might be something wrong with the logic with TripPassengerResource. Code is also not documented at the moment

---
+++ base/server/src/test/java/se/lth/base/server/tripPassenger/TripPassengerDataAccessTest.java
@@ -0,0 +1,27 @@
+package se.lth.base.server.tripPassenger;
+
+
+import org.junit.Test;
+import se.lth.base.server.Config;
+import se.lth.base.server.database.BaseDataAccessTest;
+import se.lth.base.server.trip.Trip;
+import se.lth.base.server.trip.TripDataAccess;
+
+import static org.junit.Assert.assertEquals;
+
+import java.sql.Timestamp;
+
+public class TripPassengerDataAccessTest extends BaseDataAccessTest {
+
+    private TripPassengerDataAccess tripPassengerDao = new TripPassengerDataAccess(Config.instance().getDatabaseDriver());
+    private TripDataAccess tripDao = new TripDataAccess(Config.instance().getDatabaseDriver());
+
+    @Test
+    public void bookTrip() {
+        Trip trip = new Trip(1, 1, 1, 2, 1, 2, 2);
+        tripDao.addTrip(1, trip);
+        TripPassenger tripPassenger = tripPassengerDao.bookTrip(trip.getId(), TEST.getId());
+        assertEquals(TEST.getId(), tripPassenger.getPassengerId());
+        assertEquals(1, tripPassenger.getTripId());
+    }
+}
---
+++ base/server/src/test/java/se/lth/base/server/tripPassenger/TripPassengerResourceTest.java
@@ -0,0 +1,46 @@
+package se.lth.base.server.tripPassenger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.Timestamp;
+
+import org.junit.Before;
+import org.junit.Test;
+import se.lth.base.server.BaseResourceTest;
+import se.lth.base.server.Config;
+import se.lth.base.server.trip.Trip;
+
+import org.junit.Before;
+
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+
+import static junit.framework.TestCase.assertEquals;
+
+
+public class TripPassengerResourceTest extends BaseResourceTest{
+
+
+    @Before
+    public void loginTest() {
+        login(TEST_CREDENTIALS);
+    }
+
+    @Test
+    public void bookTrip() {
+        
+        /* 
+        Trip trip = new Trip(1, 1, 1, 2, new Timestamp(10), new Timestamp(20), 2);
+
+        Entity<Trip> e = Entity.entity(trip, MediaType.APPLICATION_JSON);
+
+        TripPassenger t = target("tripPassenger").request().post(e, TripPassenger.class);
+
+        assertEquals((Integer) TEST.getId(), t.getPassengerId());
+        assertEquals((Integer) 1, t.getTripId());
+
+        */
+
+    }
+
+}
