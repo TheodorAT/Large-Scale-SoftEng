3a21952adc49f707cd0221b13dde8c30f4141b4a
antontttt <89006432+antontttt@users.noreply.github.com>
2023-09-21 23:05:37 +0200

    [ETS-1087] Done with bookSeat backend, just missing documentation

--- base/server/src/test/java/se/lth/base/server/tripPassenger/TripPassengerResourceTest.java
+++ base/server/src/test/java/se/lth/base/server/tripPassenger/TripPassengerResourceTest.java
@@ -2,20 +2,13 @@ package se.lth.base.server.tripPassenger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.sql.Timestamp;
-
 import org.junit.Before;
 import org.junit.Test;
 import se.lth.base.server.BaseResourceTest;
-import se.lth.base.server.Config;
 import se.lth.base.server.trip.Trip;
-
-import org.junit.Before;
 
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
-
-import static junit.framework.TestCase.assertEquals;
 
 public class TripPassengerResourceTest extends BaseResourceTest {
 
@@ -27,16 +20,14 @@ public class TripPassengerResourceTest extends BaseResourceTest {
     @Test
     public void bookTrip() {
 
-        /*
-         * Trip trip = new Trip(1, 1, 1, 2, new Timestamp(10), new Timestamp(20), 2);
-         * 
-         * Entity<Trip> e = Entity.entity(trip, MediaType.APPLICATION_JSON);
-         * 
-         * TripPassenger t = target("tripPassenger").request().post(e, TripPassenger.class);
-         * 
-         * assertEquals((Integer) TEST.getId(), t.getPassengerId()); assertEquals((Integer) 1, t.getTripId());
-         * 
-         */
+        Trip t = new Trip(1, 1, 1, 2, 1, 2, 2);
+        Entity<Trip> e = Entity.entity(t, MediaType.APPLICATION_JSON);
+        target("trip").request().post(e, Trip.class);
+
+        TripPassenger tripPassenger = target("tripPassenger").request().post(e, TripPassenger.class);
+
+        assertEquals((Integer) TEST.getId(), tripPassenger.getPassengerId());
+        assertEquals((Integer) 1, tripPassenger.getTripId());
 
     }
 
