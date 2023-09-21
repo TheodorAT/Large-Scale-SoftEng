6ab472f714c04b574be775c07caaaf45e67a816b
emad <emad_aldeen.issawi.5771@student.lu.se>
2023-09-21 10:43:05 +0200

    story Ets-987 added search.js

---
+++ base/server/src/main/resources/webassets/searchForAvailbleShuttles/search.js
@@ -0,0 +1,9 @@
+var base = base || {};
+base.searchShuttlesController = function() {
+    const controller = {};
+    //TODO: the controller object needs a load function
+    controller.load = function() {
+        //TODO
+    };
+    return controller;
+};
