2f119417de435a661ad9d50eb23c25a72e73d1d4
biancawidstam <biancawidstam@hotmail.com>
2023-09-25 10:43:42 +0200

    [ETS-1268] Formatting

--- base/server/src/main/resources/webassets/spec/driver-trips-spec.js
+++ base/server/src/main/resources/webassets/spec/driver-trips-spec.js
@@ -8,7 +8,7 @@ const locations = [
   new base.Location({ locationId: 2, municipality: "Halland", name: "Halmstad", latitude: 10030, longitude: 23313 }),
   new base.Location({ locationId: 3, municipality: "Skåne", name: "Malmö", latitude: 10000, longitude: 23213 }),
   new base.Location({ locationId: 4, municipality: "Halland", name: "Laholm", latitude: 10030, longitude: 23313 }),
-]; 
+];
 
 describe("driverTripController", function () {
   // (int id, int driverId, int fromLocationId, int toLocationId, long startTime, long endTime,int seatCapacity)
@@ -93,37 +93,36 @@ describe("driverTripController", function () {
   });
 
   describe("submit trip specs", function () {
-
     beforeEach(function (done) {
       controller.load();
       locationPromise
-      .then(function () {
-        expect(base.rest.getLocations).toHaveBeenCalled();
-      })
-      .then(function () {
-        expect(base.rest.getDriverTrips).toHaveBeenCalled();
-      })
-      .finally(done);
+        .then(function () {
+          expect(base.rest.getLocations).toHaveBeenCalled();
+        })
+        .then(function () {
+          expect(base.rest.getDriverTrips).toHaveBeenCalled();
+        })
+        .finally(done);
     });
 
-  it("should call submitDriverTrip after button click", function () {
-    const form = { from: "Malmö, Skåne", to: "Lund, Skåne", seats: 1, startTime: "2018-06-12T19:30" };
-    spyOn(controller, "submitDriverTrip");
-    spyOn(controller, "getLocationId").and.returnValues(1,3);
-    document.getElementById("from").value = form.from;
-    document.getElementById("to").value = form.to;
-    document.getElementById("seats").value = form.seats;
-    document.getElementById("startTime").value = form.startTime;
-    document.getElementById("registerbtn").click();
-    expect(controller.getLocationId).toHaveBeenCalledWith("Malmö, Skåne");
-    expect(controller.getLocationId).toHaveBeenCalledWith("Lund, Skåne");
-    expect(controller.submitDriverTrip).toHaveBeenCalledWith();
-  });
+    it("should call submitDriverTrip after button click", function () {
+      const form = { from: "Malmö, Skåne", to: "Lund, Skåne", seats: 1, startTime: "2018-06-12T19:30" };
+      spyOn(controller, "submitDriverTrip");
+      spyOn(controller, "getLocationId").and.returnValues(1, 3);
+      document.getElementById("from").value = form.from;
+      document.getElementById("to").value = form.to;
+      document.getElementById("seats").value = form.seats;
+      document.getElementById("startTime").value = form.startTime;
+      document.getElementById("registerbtn").click();
+      expect(controller.getLocationId).toHaveBeenCalledWith("Malmö, Skåne");
+      expect(controller.getLocationId).toHaveBeenCalledWith("Lund, Skåne");
+      expect(controller.submitDriverTrip).toHaveBeenCalledWith();
+    });
 
-  it("should not submit if there is no input in form", function () {
-    spyOn(controller, "submitDriverTrip");
-    node.querySelector("button").click();
-    expect(controller.submitDriverTrip).not.toHaveBeenCalled();
+    it("should not submit if there is no input in form", function () {
+      spyOn(controller, "submitDriverTrip");
+      node.querySelector("button").click();
+      expect(controller.submitDriverTrip).not.toHaveBeenCalled();
+    });
   });
-});
 });
