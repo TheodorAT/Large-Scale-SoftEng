0737dc147e43655844620c8b7149c4ff31cb0782
Amanda Nystedt <amandanystedt@gmail.com>
2023-09-26 14:06:21 +0200

    [ETS-1214] formatted code

--- base/server/src/main/resources/webassets/admin/user-admin.js
+++ base/server/src/main/resources/webassets/admin/user-admin.js
@@ -9,9 +9,9 @@ base.userAdminController = function () {
     const viewModel = this;
 
     this.render = function (template) {
-        this.update(template.content.querySelector("tr"));
-        const clone = document.importNode(template.content, true);
-        template.parentElement.appendChild(clone);
+      this.update(template.content.querySelector("tr"));
+      const clone = document.importNode(template.content, true);
+      template.parentElement.appendChild(clone);
     };
     this.update = function (trElement) {
       const td = trElement.children;
@@ -20,26 +20,26 @@ base.userAdminController = function () {
       td[2].textContent = viewModel.user.id;
       td[3].textContent = viewModel.user.role.name;
     };
-  }
+  };
   const view = {
     // Creates HTML for each trip in model
     render: function () {
-          // A template element is a special element used only to add dynamic content multiple times.
-          // See: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/template
-          const t = this.template();
-          model.forEach((d) => d.render(t));
+      // A template element is a special element used only to add dynamic content multiple times.
+      // See: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/template
+      const t = this.template();
+      model.forEach((d) => d.render(t));
     },
-        template: function () {
-          return document.getElementById("user-template");
-        },
+    template: function () {
+      return document.getElementById("user-template");
+    },
   };
   const controller = {
     load: function () {
-        base.rest.getUsers().then(function (users) {
+      base.rest.getUsers().then(function (users) {
         model = users.map((f) => new UserViewModel(f));
         view.render();
-        });
-    }
-};
+      });
+    },
+  };
   return controller;
 };
