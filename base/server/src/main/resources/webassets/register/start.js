96bc34bc5005907a49305e8755f5c2213fd8cf95
Max Emtefall <ma3452em-s@student.lu.se>
2023-09-26 13:09:33 +0200

    Prettier formatation

--- base/server/src/main/resources/webassets/register/start.js
+++ base/server/src/main/resources/webassets/register/start.js
@@ -1,21 +1,18 @@
-var base = base || {};
+//TODO add javadoc
 
+var base = base || {};
 
 let loginButton = document.getElementById("login-button");
 let createButton = document.getElementById("create-button");
 
-    // Add a click event listener to the login button
-    loginButton.addEventListener("click", function() {
-        // Redirect to the second page
-        base.changeLocation("/login/login.html")
-    });
+loginButton.addEventListener("click", function () {
+  base.changeLocation("/login/login.html");
+});
 
-    // Add a click event listener to the login button
-    createButton.addEventListener("click", function() {
-        // Redirect to the second page
-        base.changeLocation("/register/register.html")
-    });
+createButton.addEventListener("click", function () {
+  base.changeLocation("/register/register.html");
+});
 
-    base.changeLocation = function (url) {
-        window.location.replace(url);
-      };
+base.changeLocation = function (url) {
+  window.location.replace(url);
+};
