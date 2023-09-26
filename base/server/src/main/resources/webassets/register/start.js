f7510f4da1faa75f2dcfee7fb6cffd2b1c1898d9
justin hellsten <justin.hellsten@hotmail.com>
2023-09-26 10:52:50 +0200

    Some files for the startpage

---
+++ base/server/src/main/resources/webassets/register/start.js
@@ -0,0 +1,7 @@
+var loginButton = document.getElementById("login-button");
+
+    // Add a click event listener to the login button
+    loginButton.addEventListener("click", function() {
+        // Redirect to the second page
+        window.location.href = "register/login.html"; // Replace with the actual URL of your second page
+    });
