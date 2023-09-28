ad9a342fc82fad9481619d4076efe8fc824ef36d
justin hellsten <justin.hellsten@hotmail.com>
2023-09-28 15:03:51 +0200

    ETS-1037 passing all backend tests, removed unnecessary test

--- base/server/src/test/java/se/lth/base/server/BaseResourceTest.java
+++ base/server/src/test/java/se/lth/base/server/BaseResourceTest.java
@@ -5,7 +5,6 @@ import org.glassfish.jersey.test.JerseyTest;
 import org.junit.After;
 import org.junit.Before;
 import se.lth.base.server.database.CreateSchema;
-import se.lth.base.server.user.UserResource;
 import se.lth.base.server.providers.JsonProvider;
 import se.lth.base.server.user.*;
 
@@ -25,6 +24,16 @@ public class BaseResourceTest extends JerseyTest {
     protected static final User TEST = new User(2, Role.USER, "Test", "TestUser", "TestUser", "test@user.se",
             "+4600000000");
     protected static final Credentials TEST_CREDENTIALS = new Credentials("Test", "password", Role.USER);
+    protected static final Credentials TEST_CREDENTIALS_REGISTER = new Credentials(
+        "RandomUsername",
+        "RandomPassword",
+        Role.USER,
+        "RandomFirstName",
+        "RandomLastName",
+        "random@example.com",
+        "+4600000000"
+    );
+
 
     private static final String IN_MEM_DRIVER_URL = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
 
