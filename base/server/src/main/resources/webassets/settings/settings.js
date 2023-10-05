5cb6e01e95defcb6688c44f130abdd8104c8ffe6
biancawidstam <biancawidstam@hotmail.com>
2023-10-06 00:21:19 +0200

    [ETS-616] Downloaded Max and Justin's Settings folder that they sent and added to the application, changed some parts such as used bootstrap instead of using css to style the page, created a settingsController for js-file (empty for now).

---
+++ base/server/src/main/resources/webassets/settings/settings.js
@@ -0,0 +1,27 @@
+/*
+ * Model/view/controller for the Settings tab.
+ * Author: ..
+ */
+var base = base || {};
+// Defines the base namespace, if not already declared. Through this pattern it doesn't matter which order
+// the scripts are loaded in.
+
+base.settingsController = function() {
+    'use strict' // add this to avoid some potential bugs
+
+
+    const view = {
+        render: function() {
+        
+        },
+
+    };
+
+    const controller = {
+        load: function() {
+           
+        },
+    };
+
+    return controller;
+};
