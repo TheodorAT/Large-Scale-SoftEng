b8ffce86383eaa8c2c38d650f740d6a71b450f5f
biancawidstam <biancawidstam@hotmail.com>
2023-10-05 22:39:19 +0200

    [ETS-1282] Tests done for MyTrip-page

--- base/server/src/main/resources/webassets/spec/mytrips-spec.js
+++ base/server/src/main/resources/webassets/spec/mytrips-spec.js
@@ -72,9 +72,9 @@ describe("myTripsController", function () {
     spyOn(base.rest, "getLocations").and.returnValue(locationPromise);
     adminPromise = Promise.resolve(admin);
     spyOn(base.rest, "getUser").and.returnValues(adminPromise);
-    tripDriverPromise = Promise.resolve(trips.slice(0, 1));
+    tripDriverPromise = Promise.resolve(trips.slice(0, 2));
     spyOn(base.rest, "getDriverTrips").and.returnValue(tripDriverPromise);
-    tripPassengerPromise = Promise.resolve(trips.slice(1, 2));
+    tripPassengerPromise = Promise.resolve(trips.slice(1, 3));
     spyOn(base.rest, "getPassengerTrips").and.returnValue(tripPassengerPromise);
   });
   // Remove the node from the DOM
@@ -89,21 +89,17 @@ describe("myTripsController", function () {
       .then(function () {
         expect(base.rest.getLocations).toHaveBeenCalled();
         expect(base.rest.getUser).toHaveBeenCalled();
-        tripDriverPromise
+        Promise.all([tripDriverPromise, tripPassengerPromise])
           .then(function () {
-            expect(base.rest.getDriverTrips).toHaveBeenCalled();
-          })
-          .finally(done);
-        tripPassengerPromise
-          .then(function () {
-            expect(base.rest.getPassengerTrips).toHaveBeenCalled();
+            const rows = node.querySelectorAll("tbody tr");
+            expect(rows.length).toBe(trips.length);
           })
           .finally(done);
       })
       .finally(done);
   });
 
-  it("should fetch passenger trips on load when passenger", function (done) {
+  it("should only fetch passenger trips on load when passenger", function (done) {
     userPromise = Promise.resolve(test);
     base.rest.getUser = jasmine.createSpy().and.returnValue(userPromise);
     controller.load();
@@ -115,6 +111,8 @@ describe("myTripsController", function () {
         tripDriverPromise
           .then(function () {
             expect(base.rest.getDriverTrips).not.toHaveBeenCalled();
+            const rows = node.querySelectorAll("tbody tr");
+            expect(rows.length).toBe(trips.length / 2);
           })
           .finally(done);
         tripPassengerPromise
@@ -125,88 +123,24 @@ describe("myTripsController", function () {
       })
       .finally(done);
   });
-  /*
 
-    it('should populate table on load', function(done) {
-        const foosPromise = Promise.resolve(foos.slice(0));
-        spyOn(base.rest, 'getFoos').and.returnValue(foosPromise);
-        controller.load();
-        foosPromise.then(function() {
-            // it should have populated the table with three rows
-            const rows = node.querySelectorAll('tbody tr');
-            expect(rows.length).toBe(foos.length);
-        }).finally(done);
-    });
-
-    // Change here for lab 1
-    it('should have total in table', function(done) {
-        const foosPromise = Promise.resolve([foos[0]]);
-        spyOn(base.rest, 'getFoos').and.returnValue(foosPromise);
-        controller.load();
-        foosPromise.then(function() {
-            const tr = node.querySelector('tbody tr');
-            const tds = tr.querySelectorAll('td');
-            expect(tds.length).toBe(3);
-            expect(tds[0].textContent).toBe(foos[0].payload);
-            const d = foos[0].createdDate;
-            expect(tds[1].textContent).toBe(d.toLocaleDateString() + ' ' + d.toLocaleTimeString());
-            expect(tds[2].textContent).toBe(''+foos[0].total);
-        }).finally(done);
-     });
-
-    describe('submit specs', function() {
-        const mockFoo = new base.Foo({id: 4, userId: 1, payload: 'new data', created: 3});
-        let fooPromise;
-
-        beforeEach(function(done) {
-            const foosPromise = Promise.resolve([]);
-            fooPromise = Promise.resolve(mockFoo);
-            spyOn(base.rest, 'getFoos').and.returnValue(foosPromise);
-            spyOn(base.rest, 'addFoo').and.returnValue(fooPromise);
-            controller.load();
-            foosPromise.finally(done);
-        });
-
-        it('should call submitFoo after button click', function() {
-            spyOn(controller, 'submitFoo');
-            node.querySelector('input').value = mockFoo.payload;
-            node.querySelector('button').click();
-            expect(controller.submitFoo).toHaveBeenCalledWith();
-        });
-
-        it('should call rest api with foo', function(done) {
-            node.querySelector('input').value = mockFoo.payload;
-            node.querySelector('button').click();
-            fooPromise.then(function() {
-                expect(base.rest.addFoo).toHaveBeenCalledWith({payload: 'new data'});
-            }).finally(done);
-        });
-
-        it('should not submit if there is no text in input', function() {
-            spyOn(controller, 'submitFoo');
-            node.querySelector('button').click();
-            expect(controller.submitFoo).not.toHaveBeenCalled();
-        });
-
-        it('should add row after submit', function(done) {
-            const input = node.querySelector('input');
-            input.value = mockFoo.payload;
-            controller.submitFoo(input);
-            fooPromise.then(function() {
-                expect(node.querySelectorAll('tbody tr').length).toBe(1);
-                expect(node.querySelector('tbody td').textContent).toBe('new data');
-            }).finally(done);
-        });
-
-        it('should clear input after submit', function(done) {
-            const input = node.querySelector('input');
-            input.value = mockFoo.payload;
-            controller.submitFoo(input);
-            fooPromise.then(function() {
-                expect(input.value).toBe('');
-                expect(node.querySelectorAll('tbody tr').length).toBe(1);
-                expect(node.querySelector('tbody td').textContent).toBe('new data');
-            }).finally(done);
-        });
-    }); */
+  it("should have display destination and origin in table", function (done) {
+    controller.load();
+    let promise = [locationPromise, adminPromise];
+    Promise.all(promise)
+      .then(function () {
+        expect(base.rest.getLocations).toHaveBeenCalled();
+        expect(base.rest.getUser).toHaveBeenCalled();
+        Promise.all([tripDriverPromise, tripPassengerPromise])
+          .then(function () {
+            const tr = node.querySelector("tbody tr");
+            const tds = tr.querySelectorAll("td");
+            expect(tds.length).toBe(8);
+            expect(tds[0].textContent).toBe(locations[0].name + ", " + locations[0].municipality);
+            expect(tds[1].textContent).toBe(locations[1].name + ", " + locations[1].municipality);
+          })
+          .finally(done);
+      })
+      .finally(done);
+  });
 });
