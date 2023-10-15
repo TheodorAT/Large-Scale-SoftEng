35128216644b7352772b8f8249248c6e91f4a647
biancawidstam <biancawidstam@hotmail.com>
2023-10-15 13:49:34 +0200

    [ETS-1340] Added getRequestedTrips in rest.js, changed order in routingTable to avoid routing to  search avaiable trips when logging in as driver

--- base/server/src/main/resources/webassets/mytrips/my-trips.js
+++ base/server/src/main/resources/webassets/mytrips/my-trips.js
@@ -47,24 +47,18 @@ base.myTripsController = function () {
       switch (viewModel.trip.status_id) {
         case 1:
           //ACTIVE(1) if active it should display a cancel button
-          button = view.createButton(viewModel.trip.id, "Cancel",  "btn-danger");
+          button = view.createButton(viewModel.trip.id, "Cancel", "btn-danger");
           break;
         case 2:
-          //CANCELLED(2)  if cancelled, should be able to book - same as requested?? both without driver
-          if (viewModel.trip.startTime > now) {
-            button = view.createStatusButtons(viewModel.trip.id, "Cancelled", "bg-danger");
-          } else {
-            button = view.createStatusButtons(viewModel.trip.id, "Completed", "bg-success");
-          }
+          //CANCELLED(2)  if cancelled, it should display cancelled
+          button = view.createStatusButtons(viewModel.trip.id, "Cancelled", "bg-danger");
           break;
         case 3:
-        // REQUESTED(3) if user
+          // REQUESTED(3) if user
           button = view.createStatusButtons(viewModel.trip.id, "Requested", "bg-primary");
           break;
-        default:
-          button = view.createStatusButtons(viewModel.trip.id, "Completed", "bg-success");
       }
-      if (viewModel.trip.startTime < now) {
+      if (viewModel.trip.startTime < now && viewModel.trip.status_id != "CANCELLED") {
         button = view.createStatusButtons(viewModel.trip.id, "Completed", "bg-success");
       }
       td[7].children[0] ? td[7].children[0].replaceWith(button) : td[7].appendChild(button);
@@ -89,7 +83,7 @@ base.myTripsController = function () {
       let button = document.createElement("button");
       button.innerHTML = title;
       button.id = id;
-      button.classList.add("btn",type);
+      button.classList.add("btn", type);
       return button;
     },
     createStatusButtons: function (id, title, type) {
@@ -111,18 +105,32 @@ base.myTripsController = function () {
         currentUser = array[0];
         locations = array[1];
         let role = currentUser.role.name;
-        //Admin gets all trips, should not be possible to book yourself as passenger if you are a driver, therefore no duplicates
-        if (role == "DRIVER" || role == "ADMIN") {
-          base.rest.getDriverTrips().then(function (trips) {
-            model = trips.map((f) => new MyTripsViewModel(f));
-            view.render();
-          });
-        }
-        if (role == "USER" || role == "ADMIN") {
-          base.rest.getPassengerTrips().then(function (trips) {
-            model = trips.map((f) => new MyTripsViewModel(f));
-            view.render();
-          });
+        let driverPromise = base.rest.getDriverTrips();
+        let passengerPromise = base.rest.getPassengerTrips();
+        //Admin gets all trips
+        switch (role) {
+          case "ADMIN":
+            Promise.all([driverPromise, passengerPromise]).then(function (array) {
+              let driverTrips = array[0];
+              let passengerTrips = array[1];
+              // Merge driverTrips and passengerTrips into the model array
+              let trips = driverTrips.concat(passengerTrips);
+              // Create MyTripsViewModel instances for each trip
+              model = trips.map((trip) => new MyTripsViewModel(trip));
+              view.render();
+            });
+            break;
+          case "DRIVER":
+            driverPromise.then(function (trips) {
+              model = trips.map((f) => new MyTripsViewModel(f));
+              view.render();
+            });
+            break;
+          case "USER":
+            passengerPromise.getPassengerTrips().then(function (trips) {
+              model = trips.map((f) => new MyTripsViewModel(f));
+              view.render();
+            });
         }
       });
     },
@@ -135,7 +143,7 @@ base.myTripsController = function () {
         (b) =>
           (b.onclick = function (event) {
             //If the user is a passenger on the trip, delete passengerTrip and remove row
-            //If the user is the driver on the trip, cancel driverTrip and remove row (should be in requested)
+            //If the user is the driver on the trip, cancel driverTrip and remove row (should be in requested if passengers)
             // Cancels the trips from the server through the REST API, see rest.js for definition.
             let tripRow = document.getElementById(event.target.id).parentNode.parentNode;
             let driverId = tripRow.children[6].id;
